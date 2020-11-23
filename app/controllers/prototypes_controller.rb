0fcaab2215eae2fc1522458b02b66fc3b5d30e76
inoue-souta <73748586+inoue-souta@users.noreply.github.com>
2020-11-23 15:54:27 +0900

    プロトの編集不具合の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -36,10 +36,10 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if  prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to root_path
-    else 
+    else
       render :edit
     end
   end
